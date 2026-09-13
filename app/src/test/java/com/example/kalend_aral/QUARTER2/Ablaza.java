64b1fdf2ab8ec8cc4cacbbde3441a1a17e3e35d4
ablaza-arkeen-stephen <arkeenstephena@gmail.com>
2026-09-13 21:41:26 +0800

    testfile peta 2 ablaza

---
+++ app/src/test/java/com/example/kalend_aral/QUARTER2/Ablaza.java
@@ -0,0 +1,21 @@
+package com.example.kalend_aral.QUARTER2;
+
+import org.junit.Test;
+
+public class Ablaza {
+    @Test
+    public void printMyProfile() {
+        String myName = "Arkeen";
+        String petName = "Rene";
+        String favFood = "Lechon Kawali";
+        int myAge = 16;
+
+
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is " + myName + "and I'am " + myAge + " years Old.");
+        System.out.println(" I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day.");
+
+    }
+}
