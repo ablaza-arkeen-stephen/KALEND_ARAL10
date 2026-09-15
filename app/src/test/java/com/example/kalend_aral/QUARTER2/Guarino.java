cd9d7fb420ea1df83324d140bb46dc88f29fcee4
teb-guarino <guarinocharlestebene@gmail.com>
2026-09-15 15:32:19 +0800

    done

--- app/src/test/java/com/example/kalend_aral/QUARTER2/Guarino.java
+++ app/src/test/java/com/example/kalend_aral/QUARTER2/Guarino.java
@@ -1,4 +1,20 @@
 package com.example.kalend_aral.QUARTER2;
-
+import org.junit.Test;
 public class Guarino {
+
+    @Test
+    public void printMYprofile(){
+        String myName = "Teb";
+        String petname1 = "nguso";
+        String petname2 = "ipin";
+        String favfood = "Chicken";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");;
+        System.out.println("Hello my name is " + myName + " and I am + myAge +");
+        System.out.println("I have 2 wonderful pets named" + petname1 + " and " + petname2 + ".");
+        System.out.println("If i could eat " + favfood + " everyday!");
+
+    }
+
 }
