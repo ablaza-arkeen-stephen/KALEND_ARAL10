94f5a476bbcac3f537c3cacb683e635a7c691a37
teb-guarino <guarinocharlestebene@gmail.com>
2026-09-15 15:31:37 +0800

    Added Almasco's profile test

---
+++ app/src/test/java/com/example/kalend_aral/QUARTER2/Almasco.java
@@ -0,0 +1,22 @@
+package com.example.kalend_aral.QUARTER2;
+
+import org.junit.Test;
+
+public class Almasco {
+        @Test
+        public void printMyProfile() {
+            String myName = "Arvin";
+            String petName = "Kaiju";
+            String favFood = "Binangkal";
+            int myAge = 17;
+
+
+
+            System.out.println("---MY DIGITAL PROFILE---");
+            System.out.println("Hello, my name is " + myName + " and I'am " + myAge + " years Old.");
+            System.out.println("I have a wonderful pet named " + petName + ".");
+            System.out.println("If I could, I would eat " + favFood + " every single day.");
+
+        }
+    }
+
